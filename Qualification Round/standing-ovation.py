2a2bb3240039ef410085d18b8b57d9bdeaad227a
kamyu <kamyu104@users.noreply.github.com>
2015-04-14 13:01:50 +0800

    Update standing-ovation.py

--- Qualification Round/standing-ovation.py
+++ Qualification Round/standing-ovation.py
@@ -3,16 +3,16 @@
 def solve():
     num, shy = raw_input().split()
     num = int(num)
-    ans = 0
-    cur = int(shy[0])
+    friends = 0
+    stand = int(shy[0])
     for i in xrange(1, num + 1):
         # make sure current standing people are enough to make shy[i] stand up, 
         # if not, add friends
-        if i > cur:
-            ans += i-cur
-            cur += i-cur
-        cur += int(shy[i])
-    return ans
+        if i > stand:
+            friends += i-stand
+            stand += i-stand
+        stand += int(shy[i])
+    return friends
 
 T = input()
 for i in xrange(T):
