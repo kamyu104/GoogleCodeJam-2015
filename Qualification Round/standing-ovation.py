6744ba2b7d1321a8493bfa4220887fe2f0d177f1
kamyu <kamyu104@users.noreply.github.com>
2015-04-14 12:59:58 +0800

    Update standing-ovation.py

--- Qualification Round/standing-ovation.py
+++ Qualification Round/standing-ovation.py
@@ -1,15 +1,17 @@
 # Problem Description: https://code.google.com/codejam/contest/6224486/dashboard#s=p0
 
 def solve():
-    num, level = raw_input().split()
+    num, shy = raw_input().split()
     num = int(num)
     ans = 0
-    cur = int(level[0])
+    cur = int(shy[0])
     for i in xrange(1, num + 1):
+        # make sure current standing people are enough to make shy[i] stand up, 
+        # if not, add friends
         if i > cur:
             ans += i-cur
             cur += i-cur
-        cur += int(level[i])
+        cur += int(shy[i])
     return ans
 
 T = input()
