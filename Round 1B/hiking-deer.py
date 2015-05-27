39250937b58033b7d066511ad7dbfeb37f98cfda
Allen Liu <kamyu104@gmail.com>
2015-05-28 03:41:31 +0800

    add solution

---
+++ Round 1B/hiking-deer.py
@@ -0,0 +1,28 @@
+# https://code.google.com/codejam/contest/8224486/dashboard#s=p2
+# Time:  O(HlogH)
+# Space: O(H)
+
+import heapq
+
+for case in xrange(input()):
+    heap = []
+    hikers = 0
+    for i in xrange(input()):
+        d, h, m = map(int, raw_input().strip().split())
+        for circle_t in xrange(m, m+h):
+            t = circle_t * (360-d) / 360.0
+            heapq.heappush(heap, (t, 1, circle_t))
+            hikers += 1
+
+    current = hikers
+    result = current
+    # At most 2H times to find min encounters.
+    # undertake <= H, overtake <= H,
+    # current = undertak + overtack <= 2H
+    while current <= hikers*2:
+        t, d, circle_t = heapq.heappop(heap)
+        current -= d
+        result = min(result, current)
+        heapq.heappush(heap, (t+circle_t, -1, circle_t))
+
+    print("Case #%d: %s" % (case+1, result))
