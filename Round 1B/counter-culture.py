05c6af38cab53f5480153fc3a41990b128baba1a
Allen Liu <kamyu104@gmail.com>
2015-05-27 23:24:12 +0800

    update

--- Round 1B/counter-culture.py
+++ Round 1B/counter-culture.py
@@ -1,4 +1,7 @@
 # https://code.google.com/codejam/contest/8224486/dashboard
+# Time:  O(nlogn)
+# Space: O(1)
+
 import sys
 
 def reverse(nr):
@@ -9,18 +12,17 @@ for case in xrange(input()):
     count = 0
     while n > 0:
         digits = str(n)
-        halfcount = (len(digits)+1)//2
-        rev = reverse(n)
-        mod = n % (10 ** halfcount)
+        half_count = (len(digits)+1)//2
+        rev_n = reverse(n)
+        mod = n % (10 ** half_count)
 
-        if mod == 1 and n != rev:
-            n = rev
+        if mod == 1 and n != rev_n: # 999001 => 100999
+            n = rev_n
             count += 1
-        elif mod <= 1:
+        elif mod <= 1: # 100001 => 100000, 100000 => 99999
             n -= 1
             count += 1
-        else:
+        else: # 100999 => 100001
             n -= (mod-1)
             count += (mod-1)
-
     print("Case #%d: %s" % (case+1, count))
