0ff80f41910d8d4060d8e914c0a079a31e41c07c
kamyu <kamyu104@users.noreply.github.com>
2015-05-27 23:28:53 +0800

    Update counter-culture.py

--- Round 1B/counter-culture.py
+++ Round 1B/counter-culture.py
@@ -1,11 +1,11 @@
 # https://code.google.com/codejam/contest/8224486/dashboard
-# Time:  O(nlogn)
-# Space: O(1)
+# Time:  O(logn)
+# Space: O(logn), for reversed string
 
 import sys
 
-def reverse(nr):
-    return int(str(nr)[::-1])
+def reverse(num):
+    return int(str(num)[::-1])
 
 for case in xrange(input()):
     n = int(raw_input())
@@ -13,11 +13,11 @@ for case in xrange(input()):
     while n > 0:
         digits = str(n)
         half_count = (len(digits)+1)//2
-        rev_n = reverse(n)
+        reversed_n = reverse(n)
         mod = n % (10 ** half_count)
 
-        if mod == 1 and n != rev_n: # 999001 => 100999
-            n = rev_n
+        if mod == 1 and n != reversed_n: # 999001 => 100999
+            n = reversed_n
             count += 1
         elif mod <= 1: # 100001 => 100000, 100000 => 99999
             n -= 1
