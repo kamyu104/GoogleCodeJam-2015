f284024e809113ef65a853f7b97dd6a94bc89965
kamyu <kamyu104@users.noreply.github.com>
2015-05-28 00:38:27 +0800

    Update noisy-neighbors.cpp

--- Round 1B/noisy-neighbors.cpp
+++ Round 1B/noisy-neighbors.cpp
@@ -1,11 +1,11 @@
 // Copyright (c) 2015 kamyu. All rights reserved.
 
 /*
- * UVa10340 - All in All
+ * Google Code Jam 2015 Round 1B- Noisy Neighbors
  * https://code.google.com/codejam/contest/8224486/dashboard#s=p1
  *
- * Time  : O(m), m is length of t.
- * Space : O(1)
+ * Time  : O(R * C)
+ * Space : O(R * C)
  *
  */
 
@@ -46,19 +46,20 @@ int main() {
                         }
                         ++count;
                     }
-                    edges[(i + j) % 2].push_back(count);
+                    edges[(i + j) % 2].emplace_back(count);
                 }
             }
+            // Sort by the edge number of the room.
             sort(edges[0].begin(), edges[0].end(), greater<int>());
             sort(edges[1].begin(), edges[1].end(), greater<int>());
 
-            // Max edges of even rooms.
+            // Max edge number of vacant even rooms.
             int even_edges = accumulate(edges[0].cbegin(),
                                         edges[0].cbegin() + vacant_num, 0);
-            // Max edges of odd rooms.
+            // Max edge number of vacant odd rooms.
             int odd_edges = accumulate(edges[1].cbegin(),
                                        edges[1].cbegin() + vacant_num, 0);
-            // Total edges - max edges of vacant rooms.
+            // Total edges - max edge numbers of vacant rooms.
             answer = R * (C - 1) + (R - 1) * C - max(even_edges, odd_edges);
         }
         cout << "Case #" << test << ": " << answer << endl;
