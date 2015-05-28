fc67ed7133a85ac2eaac8c97fe85c556266d4469
Allen Liu <kamyu104@gmail.com>
2015-05-29 01:25:20 +0800

    add solution

---
+++ Round 1C/typewrite-monkey.py
@@ -0,0 +1,60 @@
+# https://code.google.com/codejam/contest/4244486/dashboard#s=p1
+# Time:  O(K + L^2  + L * S)
+# Space: O(K + L)
+
+def chars_before_repeat(s):
+    L = len(s)
+    # s = ABA, res => 2 because after 2 chars we can start a new pattern already
+    for i in xrange(1, L+1):
+        if s[i:] == s[0:L-i]:
+            return i
+    return L
+
+def bananas(K, L, S, keyboard, target):
+    if L > S:
+        return 0
+    
+    # Compute keyboard frequency
+    keyboard_freq = {}
+    for k in keyboard:
+        if k in keyboard_freq:
+            keyboard_freq[k] += 1
+        else:
+            keyboard_freq[k] = 1
+
+    # Make sure all necessary letter are there
+    for k in target:
+        if k not in keyboard_freq:
+            return 0
+    
+    # First, what is the maximum number of repeats?
+    spacing = chars_before_repeat(target)
+    ideal = (S-L)//spacing + 1
+    
+    # Second, what is the average number of repeats?
+    keyboard_prob = {}
+    for k,v in keyboard_freq.iteritems():
+        keyboard_prob[k] = v / float(K)
+    
+    if L == 1:
+        return ideal - S * keyboard_prob[target[0]]
+
+    # Compute the probability of each position by DP.
+    expected = 0.0
+    running_prob = [0.0 for i in xrange(L-1)]
+    for i in xrange(S):
+        # Given i, running_prob[j] means probability of S[i-j:i+1] matching target[:j+1].
+        expected += running_prob[L-2] * keyboard_prob[target[L-1]]
+        
+        # Update the probability in current index i.
+        for j in xrange(L-2, 0, -1):
+            running_prob[j] = running_prob[j-1] * keyboard_prob[target[j]]
+        running_prob[0] = keyboard_prob[target[0]]
+    
+    return ideal - expected
+
+for case in xrange(input()):
+    K, L, S =  map(int, raw_input().strip().split())
+    keyboard = raw_input()
+    target = raw_input()
+    print "Case #{:d}: {:.10f}".format(case+1, bananas(K, L, S, keyboard, target))
