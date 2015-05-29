dac076cc56c88d878acf842e0449ddc5306edb96
kamyu <kamyu104@users.noreply.github.com>
2015-05-30 00:29:19 +0800

    Update haircut.py

--- Round 1A/haircut.py
+++ Round 1A/haircut.py
@@ -1,34 +1,46 @@
+# Copyright (c) 2015 kamyu. All rights reserved.
+#
+# Google Code Jam 2015 Round 1A - Problem B. Haircut
 # https://code.google.com/codejam/contest/4224486/dashboard#s=p1
+#
+# Time:  O(log(N * max(M)) + BlogB)
+# Space: O(B)
+#
 
-for cas in xrange(1,1+input()):
-	b, n = map(int, raw_input().strip().split())
-	m = map(int, raw_input().strip().split())
-	def f(T):
-		s = 0
-		for i in xrange(b):
-			s += (T + m[i]-1) / m[i]
-		return s
+def haircut():
+    b, n = map(int, raw_input().strip().split())
+    m = map(int, raw_input().strip().split())
+    def f(T):
+    	s = 0
+    	for i in xrange(b):
+    	    s += (T + m[i]-1) // m[i]
+	 return s
+
+    # Time:  O(log(N * max(M)))
+    # Find the most time when the number of guests is still less than n
+    L = -1
+    R = 10**14 + 1
+    while  R - L > 1:
+        M = L + R >> 1
+        fM = f(M)
+        if fM >= n:
+            R = M
+        else:
+            L = M
+    # Now f(L) < n <= f(R)
+    assert f(L) < n <= f(R)
+    time_to_available = [0]*b
+    for i in xrange(b):
+        ct = (L + m[i] - 1) / m[i]
+        time_to_available[i] = ct * m[i]
+        n -= ct
+    
+    # Time:  O(BlogB)
+    # Sort time_to_available by time and id, assign guest to the available one 
+    assert 0 <= n <= b
+    idxs = sorted(range(b), key=lambda i: (time_to_available[i], i))
 	
-	# find the most time when the number of guests is still less than n
-	L = 0
-	R = 10**50
-	while  R - L > 1:
-		M = L + R >> 1
-		fM = f(M)
-		if fM < n:
-			L = M
-		else:
-			R = M
-
-	# now f(L) < n <= f(R)
-	assert f(L) < n <= f(R)
-	time_to_available = [0]*b
-	for i in xrange(b):
-		ct = (L + m[i] - 1) / m[i]
-		time_to_available[i] = ct * m[i]
-		n -= ct
-
-	assert 0 <= n <= b
-	# sort time_to_available by time and id, assign guest to the available one 
-	idxs = sorted(range(b), key=lambda i: (time_to_available[i], i))
-	print "Case #%s: %s" % (cas, idxs[n-1]+1)
+    return idxs[n-1]+1
+	
+for case in xrange(input()):
+    print "Case #%d: %d" % (case+1, haircut())
