e9dbe78247a2a05ae2dec9fef8654b8fbdd6c03a
kamyu <kamyu104@users.noreply.github.com>
2015-05-30 00:03:41 +0800

    Update mushroom-monster.py

--- Round 1A/mushroom-monster.py
+++ Round 1A/mushroom-monster.py
@@ -1,8 +1,16 @@
+# Copyright (c) 2015 kamyu. All rights reserved.
+#
+# Google Code Jam 2015 Round 1A - Problem A. Mushroom Monster
 # https://code.google.com/codejam/contest/4224486/dashboard#s=p0
-for cas in xrange(1,1+input()):
+#
+# Time:  O(N)
+# Space: O(1)
+#
+
+for case in xrange(input()):
 	n = input()
 	m = map(int, raw_input().strip().split())
-	a = sum(max(0, m[i-1]-m[i]) for i in xrange(1,n))
-	rate = max(max(0, m[i-1]-m[i]) for i in xrange(1,n))
-	b = sum(min(rate, m[i]) for i in xrange(n-1))
-	print "Case #%d: %s %s" % (cas, a, b)
+	first = sum(max(0, m[i]-m[i+1]) for i in xrange(n-1))
+	rate = max(max(0, m[i]-m[i+1]) for i in xrange(n-1))
+	second = sum(min(rate, m[i]) for i in xrange(n-1))
+	print "Case #%d: %s %s" % (case+1, first, second)
