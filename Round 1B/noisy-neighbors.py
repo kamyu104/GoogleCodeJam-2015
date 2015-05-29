9d91899605d658abd6496dcd7c7ff5af93b34601
Allen Liu <kamyu104@gmail.com>
2015-05-29 22:12:59 +0800

    add solution

---
+++ Round 1B/noisy-neighbors.py
@@ -0,0 +1,38 @@
+# Google Code Jam 2015 Round 1B - Problem B. Noisy Neighbors
+# https://code.google.com/codejam/contest/8224486/dashboard#s=p1
+# Time:  O(R * C)
+# Space: O(R * C)
+
+def unhappiness(R, C, N):
+    answer = 0
+    if N > (R * C + 1) / 2:
+        vacant_num = R * C - N
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        edges = [[], []]
+        for i in xrange(R):
+            for j in xrange(C):
+                count = 0
+                for d in directions:
+                    if i + d[0] < 0 or i + d[0] >= R or \
+                       j + d[1] < 0 or j + d[1] >= C:
+                        continue
+                    count += 1
+                edges[(i + j) % 2].append(count);
+
+        # Sort by the edge number of the room.
+        edges[0].sort(reverse=True)
+        edges[1].sort(reverse=True)
+
+        # Max edge number of vacant even / odd rooms.
+        even_edges, odd_edges = 0, 0
+        for i in xrange(vacant_num):
+            even_edges += edges[0][i]
+            odd_edges += edges[1][i]
+
+        # (total edge number) - (max edge numbers of vacant rooms).
+        answer = (R * (C - 1) + (R - 1) * C) - max(even_edges, odd_edges)
+    return answer
+
+for case in xrange(input()):
+    R, C, N = map(int, raw_input().strip().split())
+    print("Case #%d: %s" % (case+1, unhappiness(R, C, N)))
