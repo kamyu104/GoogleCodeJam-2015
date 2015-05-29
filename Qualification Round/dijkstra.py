18ec2ff10629d1bd6eaec5fb2d873142a72f126c
Allen Liu <kamyu104@gmail.com>
2015-05-29 23:00:35 +0800

    update

--- Qualification Round/dijkstra.py
+++ Qualification Round/dijkstra.py
@@ -1,54 +1,45 @@
-# Problem Description: https://code.google.com/codejam/contest/6224486/dashboard#s=p2
+# Copyright (c) 2015 kamyu. All rights reserved.
+#
+# Google Code Jam 2015 Qualification Round - Problem C. Dijkstra
+# https://code.google.com/codejam/contest/6224486/dashboard#s=p2
+#
+# Time:  O(L)
+# Space: O(L)
+#
 
-def mul(a, b, sign):
-    if a == 'i':
-        if b == 'i':
-            return '1', sign * -1
-        elif b == 'j':
-            return 'k', sign
-        elif b == 'k':
-            return 'j', sign * -1
-    elif a == 'j':
-        if b == 'i':
-            return 'k', sign * -1
-        elif b == 'j':
-            return '1', sign * -1
-        elif b == 'k':
-            return 'i', sign
-    elif a == 'k':
-        if b == 'i':
-            return 'j', sign 
-        elif b == 'j':
-            return 'i', sign * -1
-        elif b == 'k':
-            return '1', sign * -1
-    else:
-        return b, sign
+multiplicative = { '1':{'1': '1', 'i': 'i', 'j': 'j', 'k': 'k'},
+                   'i':{'1': 'i', 'i':'-1', 'j': 'k', 'k':'-j'},
+                   'j':{'1': 'j', 'i':'-k', 'j':'-1', 'k': 'i'},
+                   'k':{'1': 'k', 'i': 'j', 'j':'-i', 'k':'-1'},
+                  '-1':{'1':'-1', 'i':'-i', 'j':'-j', 'k':'-k'},
+                  '-i':{'1':'-i', 'i': '1', 'j':'-k', 'k': 'j'},
+                  '-j':{'1':'-j', 'i': 'k', 'j': '1', 'k':'-i'},
+                  '-k':{'1':'-k', 'i':'-j', 'j': 'i', 'k': '1'}}
 
-def solve():
-    l, rep = map(int, raw_input().split())
-    s = raw_input()
+def dijkstra():
+    L, X = map(int, raw_input().split())
+    S = raw_input()
+    
+    # Every four copies of anything mutilply to 1, so we can reduce mod 4
+    # at most we need 4 copies in each of the first two splits
+    # i.e. at most 4 copies of S for mupltiplying to i, 4 copes of S to j,
+    #      and the remaining (X % 4) copies of S to k).
+    if X >= 8:
+        X = 4 + 4 + X % 4
+    S *= X
 
-    # every four copies of anything mutilply to 1, so we can reduce mod 4
-    # at most we need 4 copies in each of the first two splits (4 for i, rep % 4 for j, 4 for k).
-    if rep >= 8:
-        rep = 4 + rep % 4 + 4
+    state, a = 0, '1'
+    for b in S:
+        a = multiplicative[a][b]
+        if state == 0 and a == 'i':
+            state = 1  # (i) * remaining
+        elif state == 1 and a == 'k':
+            state = 2  # (i * j) * remaining
 
-    s *= rep
-
-    status, p, sign  = 0, '1', 1
-    for q in s:
-        p, sign = mul(p, q, sign)
-        if status == 0 and p == 'i' and sign == 1:
-            status = 1
-        elif status == 1 and p == 'k' and sign == 1:
-            status = 2
-
-    if status == 2 and p == '1' and sign == -1:
+    if state == 2 and a == '-1':
         return "YES"
     else:
         return "NO"
 
-T = input()
-for i in xrange(T):
-    print 'Case #%d: %s' % (i + 1, solve())
+for case in xrange(input()):
+    print "Case #{:d}: {:s}".format(case+1, dijkstra())
