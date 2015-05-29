6422e2fcbc1b7e35a480b5b47981f864706728ea
kamyu <kamyu104@users.noreply.github.com>
2015-05-29 23:36:45 +0800

    Update ominous-omino.py

--- Qualification Round/ominous-omino.py
+++ Qualification Round/ominous-omino.py
@@ -1,41 +1,48 @@
-# Problem Description: https://code.google.com/codejam/contest/6224486/dashboard#s=p3
+# Copyright (c) 2015 kamyu. All rights reserved.
+#
+# Google Code Jam 2015 Qualification Round - Problem D. Ominous Omino
+# https://code.google.com/codejam/contest/6224486/dashboard#s=p3
+#
+# Time:  O(1)
+# Space: O(1)
+#
 
-def solve():
-    [x, r, c]  = map(int, raw_input().split())
+def omino():
+    [X, R, C]  = map(int, raw_input().strip().split())
   
-    if x>=7:
+    if X>=7:
         #A polyomino with a monomino hole cannot be part of a tiling
         return "RICHARD"
   
-    if (r*c)%x != 0:
+    if (R*C)%X != 0:
         #The grid is not tilable by X-ominoes
         return "RICHARD"
     
-    if x > max(r,c):
+    if X > max(R,C):
         #The straight X-omino does not fit in the grid
         return "RICHARD"
 
-    if (x+1)//2 > min(r,c):
+    if (X+1)//2 > min(R,C):
         #The most even-armed V-shaped X-omino does not fit in the grid
         # x
         # x
         # xxx
         return "RICHARD"
 
-    if x==4 and min(r,c)==2:
+    if X==4 and min(R,C)==2:
          #Richard wins by picking thie S tetromino
          # xx
          #  xx
          return "RICHARD"
 
-    if x==5 and min(r,c)==3 and max(r,c)==5:
+    if X==5 and min(R,C)==3 and max(R,C)==5:
          #Richard wins by choosing the W pentomino
          # x
          # xx
          #  xx 
          return "RICHARD"
   
-    if x==6 and min(r,c)==3:
+    if X==6 and min(R,C)==3:
          #Richard wins by choosing the T with a length 4 top bar
          # xxxx
          #  x
@@ -45,6 +52,5 @@ def solve():
     #Gabriel can easily work around the tile in other cases
     return "GABRIEL"
 
-T = input()
-for i in xrange(T):
-    print 'Case #%d: %s' % (i + 1, solve())
+for case in xrange(input()):
+    print 'Case #%d: %s' % (case+1, omino())
