af1fa2b05798d01ef11ff52ecee776d6d6f7164c
Allen Liu <kamyu104@gmail.com>
2015-05-30 01:24:25 +0800

    update

--- Round 1A/haircut.py
+++ Round 1A/haircut.py
@@ -10,36 +10,40 @@
 def haircut():
     b, n = map(int, raw_input().strip().split())
     m = map(int, raw_input().strip().split())
-    def f(T):
+    # Count the number of coming guests at the time T.
+    def guest_num(T):
     	s = 0
     	for i in xrange(b):
     	    s += (T + m[i]-1) // m[i]
-	 return s
+        return s
 
     # Time:  O(log(N * max(M)))
     # Find the most time when the number of guests is still less than n
-    L = -1
-    R = 10**14 + 1
-    while  R - L > 1:
+    L = 0
+    R = 10**14
+    while  L <= R:
         M = L + R >> 1
-        fM = f(M)
-        if fM >= n:
-            R = M
+        guest_num_M = guest_num(M)
+        if guest_num_M >= n:
+            R = M - 1
         else:
-            L = M
-    # Now f(L) < n <= f(R)
-    assert f(L) < n <= f(R)
+            L = M + 1
+
+    # Now guest_num(L-1) < n <= guest_num(L)
+    # At the time L, the number of coming guest achieves n.
     time_to_available = [0]*b
     for i in xrange(b):
-        ct = (L + m[i] - 1) // m[i]
+        ct = (L-1 + m[i] - 1) // m[i]
         time_to_available[i] = ct * m[i]
         n -= ct
     
     # Time:  O(BlogB)
-    # Sort time_to_available by time and id, assign guest to the available one 
-    assert 0 <= n <= b
-    idxs = sorted(range(b), key=lambda i: (time_to_available[i], i))
-	
+    # Sort time_to_available by time and id,
+    # assign the remaining guests to the available ones by id order at the time L.
+    # Now 0 <= n <= b
+    idxs = sorted(xrange(b), key=lambda i: (time_to_available[i], i))
+
+    # (i+1)th barber.
     return idxs[n-1]+1
 	
 for case in xrange(input()):
