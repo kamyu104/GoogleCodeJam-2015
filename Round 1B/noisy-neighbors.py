0acf503b63c7851ad8df0fdd5f7c3d2a15e407b7
Allen Liu <kamyu104@gmail.com>
2015-05-30 15:55:03 +0800

    update

--- Round 1B/noisy-neighbors.py
+++ Round 1B/noisy-neighbors.py
@@ -4,7 +4,7 @@
 # https://code.google.com/codejam/contest/8224486/dashboard#s=p1
 #
 # Time:  O(R * C)
-# Space: O(R * C)
+# Space: O(1)
 #
 
 def unhappiness(R, C, N):
@@ -12,26 +12,28 @@ def unhappiness(R, C, N):
     if N > (R * C + 1) / 2:
         vacant_num = R * C - N
         directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-        edges = [[], []]
+        rooms = [[0 for _ in xrange(4)] for _ in xrange(2)]
         for i in xrange(R):
             for j in xrange(C):
                 count = 0
                 for d in directions:
                     if i + d[0] < 0 or i + d[0] >= R or \
-                       j + d[1] < 0 or j + d[1] >= C:
+                        j + d[1] < 0 or j + d[1] >= C:
                         continue
                     count += 1
-                edges[(i + j) % 2].append(count);
-
-        # Sort by the edge number of the room.
-        edges[0].sort(reverse=True)
-        edges[1].sort(reverse=True)
+                rooms[(i+j)%2][4-count] += 1
 
         # Max edge number of vacant even / odd rooms.
-        even_edges, odd_edges = 0, 0
-        for i in xrange(vacant_num):
-            even_edges += edges[0][i]
-            odd_edges += edges[1][i]
+        even_edges, remaining = 0, vacant_num
+        for i in xrange(3):
+            even_edges += (4-i) * min(rooms[0][i], remaining)
+            remaining -= min(rooms[0][i], remaining)
+        
+        # Max edge number of vacantodd rooms.
+        odd_edges, remaining = 0, vacant_num
+        for i in xrange(4):
+            odd_edges += (4-i) * min(rooms[1][i], remaining)
+            remaining -= min(rooms[1][i], remaining)
 
         # (total edge number) - (max edge numbers of vacant rooms).
         answer = (R * (C - 1) + (R - 1) * C) - max(even_edges, odd_edges)
