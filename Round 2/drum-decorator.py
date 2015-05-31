6ed11e7180e0eeae347caf043e1f6ed40df85c8d
Allen Liu <kamyu104@gmail.com>
2015-06-01 07:58:19 +0800

    add solution

--- Round 2/drum-decorator.py
+++ Round 2/drum-decorator.py
@@ -4,7 +4,7 @@
 # https://code.google.com/codejam/contest/8234486/dashboard#s=p3
 #
 # Time:  O(R^2)
-# Space: O(R)
+# Space: O(1)
 #
 
 # In a 1xC, it must be all 2s if the previous row is all 3s
@@ -43,26 +43,27 @@ def lcm(a,b):
 def drum_decorator():
     type_1_2, type_3 = 0, 1
     R, C = map(int, raw_input().strip().split())
-    states = [{(type_1_2,1):1,(type_3,1):1}]+[{} for i in xrange(R)]
+    states = [{(type_1_2,1):1,(type_3,1):1}]+[{} for i in xrange(4)]
     for i in xrange(R):
-        for (type, degree) in states[i]:
+        for (type, degree) in states[i%4]:
             if type == type_1_2:  # previous one is the type of 1s and 2s decoration.
-                if (i + 2) <= R:  # 2 rows with 3s
-                    addin(states[i+2], (type_3, degree), states[i][(type, degree)])
+                if (i+2) <= R:  # 2 rows with 3s
+                    addin(states[(i+2)%4], (type_3, degree), states[i%4][(type, degree)])
             else:  # previous one is the type of 3s decoration.
-                if (i + 1) <= R:   # 1 rows with all 2s
-                    addin(states[i+1], (type_1_2, degree), states[i][(type, degree)])
-                if (i + 2) <= R:  # 2 rows with 1s and 2s
+                if (i+1) <= R:   # 1 rows with all 2s
+                    addin(states[(i+1)%4], (type_1_2, degree), states[i%4][(type, degree)])
+                if (i+2) <= R:  # 2 rows with 1s and 2s
                     if (C % 3) == 0:  # Type 1, 3 degree to rotate
-                        addin(states[i + 2], (type_1_2, lcm(degree, 3)), 3 * states[i][(type, degree)])
+                        addin(states[(i+2)%4], (type_1_2, lcm(degree, 3)), 3 * states[i%4][(type, degree)])
                     if (C % 6) == 0:  # Type 2, 6 degree to rotate
-                        addin(states[i + 2], (type_1_2, lcm(degree, 6)), 6 * states[i][(type, degree)])
-                if (i + 3) <= R:  # 3 rows with 1s and 2s
+                        addin(states[(i+2)%4], (type_1_2, lcm(degree, 6)), 6 * states[i%4][(type, degree)])
+                if (i+3) <= R:  # 3 rows with 1s and 2s
                     if (C % 4) == 0:  # 4 degree to rotate
-                        addin(states[i + 3], (type_1_2, lcm(degree, 4)), 4 * states[i][(type, degree)])
+                        addin(states[(i+3)%4], (type_1_2, lcm(degree, 4)), 4 * states[i%4][(type, degree)])
+        states[i%4] = {}
     ans = 0
-    for (type, degree) in states[-1]:
-        ans += states[R][(type, degree)]/degree  # Divided by rotate degree.
+    for (type, degree) in states[R%4]:
+        ans += states[R%4][(type, degree)]/degree  # Divided by rotate degree.
         ans %= 10**9+7
     return ans
 
