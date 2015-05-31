1c543b9340944444ed4c870e286d0fc00e480469
Allen Liu <kamyu104@gmail.com>
2015-06-01 03:48:14 +0800

    update

--- Round 2/kiddie-pool.py
+++ Round 2/kiddie-pool.py
@@ -9,7 +9,7 @@
 
 # Minimize max(T_i) s.t.
 # sum(R_i * T_i) = V
-# sum(R_i * C_i * T_i) = X * V => sum(R_i * (C_i - X) * T_i) = 0
+# sum(R_i * C_i * T_i) = X * V <=> sum(R_i * (C_i - X) * T_i) = 0
 
 def kiddie_pool():
     R, C = 0, 1
@@ -17,24 +17,26 @@ def kiddie_pool():
     N = int(N)
     sources = [map(float, raw_input().strip().split()) for i in xrange(N)]
     sources = [(source[0], (source[1]-X)) for source in sources]
-    T = "IMPOSSIBLE"
+    Tmax = "IMPOSSIBLE"
     
     if max((source[R]*source[C]) for source in sources) >= 0 and \
        min((source[R]*source[C]) for source in sources) <= 0:
-        T = V / sum(source[R] for source in sources)
+        Tmax = V / sum(source[R] for source in sources)
         for source in sources:
             if (source[R]*source[C]) != 0:
                 Cx = source[C]
                 # For each Cx find Tx
-                # RxTx + sum(RiTi) = V
-                # CxRxTx + sum(CiRiTi) = 0
-                # => V = sum(Ri(1 - Ci/Cx)Ti) <= Tmax * sum(Ri(1 - Ci/Cx))
-                # => Rx = V / Tmax <= sum(Ri(1 - Ci/Cx))
-                # Rx <= sum(source[R]*(1 - source[C]/Cx))
-                # To minimize Tmax, is to maximize Rx.
+                # 1. RxTx + sum(RiTi) = V
+                # 2. CxRxTx + sum(CiRiTi) = 0
+                # 1 + 2: V = sum(Ri(1 - Ci/Cx)Ti) <= Tmax * sum(Ri(1 - Ci/Cx))
+                # => V / Tmax <= sum(Ri(1 - Ci/Cx))
+                # Let Rx = sum(source[R]*(1 - source[C]/Cx))
+                # => V / Rx <= Tmax
+                # To minimize Tmax, is to maximize every Rx.
                 Rx = sum(max(0, source[R]*(1-source[C]/Cx)) for source in sources)
-                T = max(T, V / Rx)
-    return T
+                Tx = V / Rx
+                Tmax = max(Tmax, Tx)
+    return Tmax
 
 
 for case in xrange(input()):
