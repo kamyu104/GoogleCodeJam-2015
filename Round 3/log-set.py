023d37ed21f71593d1feaa1a3211699b8e88edea
Allen Liu <kamyu104@gmail.com>
2015-07-03 08:41:52 +0800

    update

--- Round 3/log-set.py
+++ Round 3/log-set.py
@@ -11,36 +11,37 @@ from math import log
 
 def log_set(P, E, F):
     G = {E[i]-E[0]:F[i] for i in xrange(P)}
-    v = []
-    nums = [E[i]-E[0] for i in xrange(P-1, -1, -1)]
-    goal = -E[0]
-    G2 = dict(G)  # clone of  G.
+    nums = []
     while len(G) > 1:
         Gk = sorted(G.keys())
         num = Gk[1]
-        v.append(num)
+        nums.append(num)
         for i in Gk:
             if (i + num) in G:
                 G[i + num] += -G[i]
             else:
                 G[i + num] = -G[i]
-        G = {a:b for (a,b) in G.items() if b != 0}
-    v = [0] * int(log(G[0], 2)) + v
-    w = []
-    for num in v[::-1]:
-        for i in sorted(G2.keys()):
-            if (i + num) in G2:
-                G2[i + num] += -G2[i]
+        G = {key:val for (key,val) in G.items() if val != 0}
+    
+    G = {E[i]-E[0]:F[i] for i in xrange(P)}
+    nums = [0] * int(log(G[0], 2)) + nums
+    goal = -E[0]
+    log_set = []
+    for num in nums[::-1]:
+        Gk = sorted(G.keys())
+        for i in Gk:
+            if (i + num) in G:
+                G[i + num] += -G[i]
             else:
-                G2[i + num] = -G2[i]
-        G2 = {a:b for (a,b) in G2.items() if b != 0}
-        if goal - num in G2:
-            w.append(-num)
+                G[i + num] = -G[i]
+        G = {key:val for (key,val) in G.items() if val != 0}
+        if goal - num in G:
+            log_set.append(-num)
             goal += -num
         else:
-            w.append(num)
-    w.sort()
-    return " ".join(map(str, w))
+            log_set.append(num)
+    log_set.sort()
+    return " ".join(map(str, log_set))
 
 for case in xrange(input()):
     # Read the input.
