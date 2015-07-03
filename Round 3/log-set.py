e9233681ecada2ba7a09e8d3f81355dc05b3e692
Allen Liu <kamyu104@gmail.com>
2015-07-03 08:24:34 +0800

    update

--- Round 3/log-set.py
+++ Round 3/log-set.py
@@ -11,18 +11,17 @@ from math import log
 
 def log_set(P, E, F):
     G = {E[i]-E[0]:F[i] for i in xrange(P)}
-    k = len(G) - 1
     v = []
     nums = [E[i]-E[0] for i in xrange(P-1, -1, -1)]
     goal = -E[0]
-    G2 = dict(G)
+    G2 = dict(G)  # clone of  G.
     while len(G) > 1:
         Gk = sorted(G.keys())
         num = Gk[1]
         v.append(num)
         for i in Gk:
             if (i + num) in G:
-                G[i + num] -= G[i]
+                G[i + num] += -G[i]
             else:
                 G[i + num] = -G[i]
         G = {a:b for (a,b) in G.items() if b != 0}
@@ -31,17 +30,17 @@ def log_set(P, E, F):
     for num in v[::-1]:
         for i in sorted(G2.keys()):
             if (i + num) in G2:
-                G2[i + num] -= G2[i]
+                G2[i + num] += -G2[i]
             else:
                 G2[i + num] = -G2[i]
         G2 = {a:b for (a,b) in G2.items() if b != 0}
         if goal - num in G2:
             w.append(-num)
-            goal -= num
+            goal += -num
         else:
             w.append(num)
     w = sorted(w)
-    return " ".join(str(k) for k in w)
+    return " ".join(str(i) for i in w)
 
 for case in xrange(input()):
     # Read the input.
