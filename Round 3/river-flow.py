e6185786e3c9d73ec10916975940eda47d100d6a
kamyu <kamyu104@users.noreply.github.com>
2015-07-10 17:14:16 +0800

    Update river-flow.py

--- Round 3/river-flow.py
+++ Round 3/river-flow.py
@@ -12,7 +12,7 @@ def possible(D, d):
     # xi represents the difference in flow between day i and
     # the previous day in the 2D-day cycle.
     x = [d[i]-d[i-1] for i in xrange(len(d))]
-    tot = 0
+    farmers = 0
     while D > 0:
         for j in xrange(D):
             if (x[j] + x[j + D]) % 2 == 1:
@@ -20,24 +20,27 @@ def possible(D, d):
             else:
                 scr = (x[j] - x[j + D]) / 2
                 if x[j] >= x[j + D]:
-                    tot += scr
+                    farmers += scr
                     d = d[:j] + [z - scr for z in d[j:j+D]] + d[j+D:2*D]
                 else:
-                    tot -= scr
+                    farmers -= scr
                     d = [z+scr for z in d[:j]] + d[j:j+D] + [z+scr for z in d[j+D:2*D]]
                 if min(d) < 0:
                     return -1
                 x[j] = (x[j] + x[j + D]) / 2
         D >>= 1
-    return tot
+    return farmers
 
 def river_flow(N, D, d):
+    # If the river flow data is not periodic with period 2D.
+    # the farmers are cheating.
     if len([i for i in xrange(N-2*D) if d[i] != d[i+2*D]]):
         return "CHEATERS!"
-    tot = possible(D, d[:2*D])
-    if tot < 0:
+    # Count farmers by first 2D river flow data.
+    farmers = possible(D, d[:2*D])
+    if farmers < 0:
         return "CHEATERS!"
-    return tot
+    return farmers
 
 for case in xrange(input()):
     # Read the input.
