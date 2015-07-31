85c977c567c05440b664305e1f6df3d114bb6b9b
kamyu <kamyu104@users.noreply.github.com>
2015-08-01 04:15:25 +0800

    add more detailed explanation

--- Round 1C/brattleship.py
+++ Round 1C/brattleship.py
@@ -12,11 +12,11 @@ def score(R, C, W):
     #    (R - 1) * (C // W)
     #
     # 2. The move to find the whole battle ship in the last row:
-    #    case C % W == 0:
-    #        C // W - 1 + W
-    #    case C % W != 0:
-    #        C // W + W
-    #    =>  (C - 1) // W + W
+    #    case (1) C % W == 0:
+    #        C // W - 1 + W = (C - 1) // W + W
+    #    case (2) C % W != 0:
+    #        C // W + W = (C - 1) // W + W
+    #    case (1) + (2) <=> (C - 1) // W + W
     return ((R - 1) * (C // W)) + ((C - 1) // W + W)
 
 for case in xrange(input()):
