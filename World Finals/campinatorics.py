ae28fc49bc6afd4bb4a24108723ff618676747f9
kamyu <kamyu104@users.noreply.github.com>
2015-10-27 23:46:50 +0800

    Update campinatorics.py

--- World Finals/campinatorics.py
+++ World Finals/campinatorics.py
@@ -14,6 +14,8 @@ fc = [1]
 for i in xrange(1, MAX_N + 1):
     fc.append((fc[-1] * i) % modulo)
 
+# C(n, k) = n! / (k!*(n - k)!) = n! * (k!*(n - k)!)^-1 (mod p)
+# For p prime, the inverse of any number x mod p is x^(p - 2) mod p (Euler's Theorem).
 fc2 = [pow(z, modulo - 2, modulo) for z in fc]
 
 der = [-1, 0, 1, 1, 3]
