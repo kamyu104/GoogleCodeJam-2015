9ace10b4141951e318176cdc058e455cd87b85a7
kamyu <kamyu104@users.noreply.github.com>
2015-10-27 23:48:26 +0800

    Update campinatorics.py

--- World Finals/campinatorics.py
+++ World Finals/campinatorics.py
@@ -14,21 +14,21 @@ fc = [1]
 for i in xrange(1, MAX_N + 1):
     fc.append((fc[-1] * i) % modulo)
 
-# C(n, k) = n! / (k!*(n - k)!) = n! * (k!*(n - k)!)^-1 (mod p)
-# For p prime, the inverse of any number x mod p is x^(p - 2) mod p (Euler's Theorem).
-fc2 = [pow(z, modulo - 2, modulo) for z in fc]
+fc2 = [pow(x, modulo - 2, modulo) for x in fc]
 
 der = [-1, 0, 1, 1, 3]
 for i in xrange(5, MAX_N + 1):
     der.append((der[i - 1] * (i - 1) - der[i - 3] * (i - 4)) % modulo)
 der = [(der[i] * fc[i] * (i - 1)) % modulo for i in xrange(len(der))]
 
-def combin(n, k):
+# C(n, k) = n! / (k!*(n - k)!) = n! * (k!*(n - k)!)^-1 (mod p)
+# For p prime, the inverse of any number x mod p is x^(p - 2) mod p (Euler's Theorem).
+def C(n, k):
     return fc[n] * fc2[k] * fc2[n - k]
 
 # f(N, X) = C(N, X)^2 * X! * ((N-X)! * Dearrangement(N-X))
 def f(N, X):
-    return ((combin(N, X) ** 2) * fc[X] * der[N - X]) % modulo
+    return ((C(N, X) ** 2) * fc[X] * der[N - X]) % modulo
 
 def campinatorics(N, X):
     return sum(f(N, i) for i in xrange(X, N + 1)) % modulo
