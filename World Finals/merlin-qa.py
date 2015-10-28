46e9d6d9534d11ae4ce9961d669a50d3a33a1d82
Allen Liu <kamyu@AllendeMBP.Home>
2015-10-29 00:00:08 +0800

    update

--- World Finals/merlin-qa.py
+++ World Finals/merlin-qa.py
@@ -1,24 +1,28 @@
+# Time:  O(M! * (N * M))
+# Space: O(N * M)
+
 from itertools import permutations
 
+# For each permutation of ingredient vector.
 def f(spells):
-    score = 0
-    for spell in spells:
-        h, v = 0, 0
-        for cost in spell:
-            v += cost
-            h = max(h, v)
-        score += h
-    return score
-
+    vals = 0
+    for spell in spells:    # Time: O(N)
+        val, costs = 0, 0
+        # Get max cost sum of spell vector from the beginning.
+        for cost in spell:  # Time: O(M)
+            costs += cost
+            val = max(val, costs)
+        vals += val
+    return vals
 
 def merlin_qa(N, M, spells):
     spells = [i for i in spells if max(i) > 0]
     if not spells:
         return 0
     largest = 0
-    for permutation in permutations(xrange(M)):
-        val = f([[spell[i] for i in permutation] for spell in spells])
-        largest = max(largest, val)
+    for permutation in permutations(xrange(M)):  # Time: O(M!)
+        vals = f([[spell[i] for i in permutation] for spell in spells])
+        largest = max(largest, vals)
     return largest
 
 for case in xrange(input()):
