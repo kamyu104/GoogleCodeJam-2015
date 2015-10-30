872ef17a33af32a895b873047c7fc633afaff90b
Allen Liu <kamyu@AllendeMacBook-Pro.local>
2015-10-31 01:48:37 +0800

    update

--- World Finals/costly-binary-search.cpp
+++ World Finals/costly-binary-search.cpp
@@ -27,41 +27,49 @@ const int MAX_N = 1e6;
 // O(log2(N) * max(cost)) ~= 20 * 9 = 180
 const int MAX_TIMES = 180;
 
+// left[t][p]: the farest position left to p which forms the range
+//             [left[t][p], p) we can cover in time up to t
 int left[MAX_TIMES + 1][MAX_N + 1];
+
+// right[t][p]: the farest position right to p which forms the range
+//              [p, right[t][p]) we can cover in time up to t
 int right[MAX_TIMES + 1][MAX_N + 1];
 
 int costly_binary_search() {
     string S;
     cin >> S;
     const int n = S.length();
-    for (int j = 0; j < n + 1; ++j) {
-        left[0][j] = j;
-        right[0][j] = j;
+    for (int p = 0; p <= n; ++p) {
+        left[0][p] = p;
+        right[0][p] = p;
     }
-    for (int c = 1; c <= MAX_TIMES; ++c) {
-        for (int j = 0; j < n + 1; ++j) {
-            left[c][j] = left[c - 1][j];
-            right[c][j] = right[c - 1][j];
+
+    // Given t time, check if there is a point where
+    // we'll be able to produce the entire array.
+    for (int t = 1; t <= MAX_TIMES; ++t) {
+        for (int p = 0; p <= n; ++p) {
+            left[t][p] = left[t - 1][p];
+            right[t][p] = right[t - 1][p];
         }
-        for (int j = 0; j < n; ++j) {
-            const int cc = c - (S[j] - '0');
-            if (cc >= 0) {
-                const int l = left[cc][j];
-                const int r = right[cc][j + 1];
-                left[c][r] = min(left[c][r], l);
-                right[c][l] = max(right[c][l], r);
+        for (int p = 0; p < n; ++p) {
+            const int tt = t - (S[p] - '0');
+            if (tt >= 0) {
+                const int l = left[tt][p];
+                const int r = right[tt][p + 1];
+                left[t][r] = min(left[t][r], l);
+                right[t][l] = max(right[t][l], r);
             }
         }
 
-        for (int j = n; j > 0; --j) {
-            left[c][j - 1] = min(left[c][j - 1], left[c][j]);
+        for (int p = n; p > 0; --p) {
+            left[t][p - 1] = min(left[t][p - 1], left[t][p]);
         }
-        for (int j = 0; j < n; ++j) {
-            right[c][j + 1] = max(right[c][j + 1], right[c][j]);
+        for (int p = 0; p < n; ++p) {
+            right[t][p + 1] = max(right[t][p + 1], right[t][p]);
         }
 
-        if (right[c][0] == n) {
-            return c;
+        if (right[t][0] == n) {
+            return t;  // Minimum time.
         }
     }
     return MAX_TIMES;
